6700a4985dac4a42d9f71a203229f4b81600e5b5
Arjun <dowhileyoucan00@gmail.com>
2026-06-21 14:36:20 +0530

    clear and consise

--- copy/Project.py
+++
@@ -1,4 +0,0 @@
-def greet(name):
-    result=f"Good morning, {name}!"
-    return result
-print(greet("Arjun"))
--- hello.py
+++
@@ -1,5 +0,0 @@
-def happy(name):
-    text=f"Hi ,hope you are doing good {name}!"
-    return text
-print(happy("tommy"))
-      
